b89452b1a740a8289f8b1a0d8b447f227766df44
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-02-26 13:15:19 -0500

    Print local.server.port in HTTP GET method of a ProductsController

--- src/main/java/com/appsdeveloperblog/estore/ProductsService/rest/ProductsController.java
+++ src/main/java/com/appsdeveloperblog/estore/ProductsService/rest/ProductsController.java
@@ -1,5 +1,7 @@
 package com.appsdeveloperblog.estore.ProductsService.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/products") // http://localhost:8080/products
 public class ProductsController {
 	
+	@Autowired
+	private Environment env;
+	
 	@PostMapping
 	public String createProduct() {
 		return "HTTP POST Handled";
@@ -18,7 +23,7 @@ public class ProductsController {
 	
 	@GetMapping
 	public String getProduct() {
-		return "HTTP GET Handled";
+		return "HTTP GET Handled " + env.getProperty("local.server.port");
 	}
 	
 	@PutMapping
