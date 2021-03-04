ddb10cc4f3b5a3ddff931e9288c4c677de1920ee
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-03-04 16:31:08 -0500

    Validating the CreateProductCommand in the ProductAggregate class

--- src/main/java/com/appsdeveloperblog/estore/ProductsService/command/ProductAggregate.java
+++ src/main/java/com/appsdeveloperblog/estore/ProductsService/command/ProductAggregate.java
@@ -1,5 +1,7 @@
 package com.appsdeveloperblog.estore.ProductsService.command;
 
+import java.math.BigDecimal;
+
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.spring.stereotype.Aggregate;
 
@@ -13,6 +15,15 @@ public class ProductAggregate {
 	@CommandHandler
 	public ProductAggregate(CreateProductCommand createProductCommand) {
 		// Validate Create Product Command
+		
+		if(createProductCommand.getPrice().compareTo(BigDecimal.ZERO) <= 0) {
+			throw new IllegalArgumentException("Price cannot be less or equal than zero");
+		}
+		
+		if(createProductCommand.getTitle() == null 
+				|| createProductCommand.getTitle().isBlank()) {
+			throw new IllegalArgumentException("Title cannot be empty");
+		}
 	}
 
 }
