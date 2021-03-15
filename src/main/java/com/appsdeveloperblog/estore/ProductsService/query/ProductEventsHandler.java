94001ad24f31e44aa769d30e5537bb15a5cc28ec
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-03-15 14:58:20 -0400

    implemented query side EventHandler method for the ProductCreatedEvent

--- src/main/java/com/appsdeveloperblog/estore/ProductsService/query/ProductEventsHandler.java
+++ src/main/java/com/appsdeveloperblog/estore/ProductsService/query/ProductEventsHandler.java
@@ -1,15 +1,29 @@
 package com.appsdeveloperblog.estore.ProductsService.query;
 
 import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
+import com.appsdeveloperblog.estore.ProductsService.core.data.ProductEntity;
+import com.appsdeveloperblog.estore.ProductsService.core.data.ProductsRepository;
 import com.appsdeveloperblog.estore.ProductsService.core.events.ProductCreatedEvent;
 
 @Component
 public class ProductEventsHandler {
+	
+	private final ProductsRepository productsRepository;
+	
+	public ProductEventsHandler(ProductsRepository productsRepository) {
+		this.productsRepository = productsRepository;
+	}
 
 	@EventHandler
 	public void on(ProductCreatedEvent event) {
+		
+		ProductEntity productEntity = new ProductEntity();
+		BeanUtils.copyProperties(event, productEntity);
+		
+		productsRepository.save(productEntity);
 		
 	}
 	
