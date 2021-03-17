1c959f262595e5a2327c47e48b380019bce77258
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-03-17 14:46:33 -0400

    Added the getProducts() method

--- src/main/java/com/appsdeveloperblog/estore/ProductsService/query/rest/ProductsQueryController.java
+++ src/main/java/com/appsdeveloperblog/estore/ProductsService/query/rest/ProductsQueryController.java
@@ -1,10 +1,18 @@
 package com.appsdeveloperblog.estore.ProductsService.query.rest;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
 public class ProductsQueryController {
+	
+	@GetMapping
+	public List<ProductRestModel> getProducts() {
+		
+	}
 
 }
