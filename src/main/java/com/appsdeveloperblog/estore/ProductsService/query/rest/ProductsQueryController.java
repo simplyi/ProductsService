349f99062cf527e041dc6767bc286a49f1ca9280
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-03-17 14:01:07 -0400

    Created ProductsQueryController class structure

---
+++ src/main/java/com/appsdeveloperblog/estore/ProductsService/query/rest/ProductsQueryController.java
@@ -0,0 +1,10 @@
+package com.appsdeveloperblog.estore.ProductsService.query.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsQueryController {
+
+}
