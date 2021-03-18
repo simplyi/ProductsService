ae19b51ec64040f2569aeb67f20cc4a99914869a
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-03-18 15:23:34 -0400

    Implemented the findProducts() QueryHandler method

--- src/main/java/com/appsdeveloperblog/estore/ProductsService/query/ProductsQueryHandler.java
+++ src/main/java/com/appsdeveloperblog/estore/ProductsService/query/ProductsQueryHandler.java
@@ -1,8 +1,15 @@
 package com.appsdeveloperblog.estore.ProductsService.query;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
+import com.appsdeveloperblog.estore.ProductsService.core.data.ProductEntity;
 import com.appsdeveloperblog.estore.ProductsService.core.data.ProductsRepository;
+import com.appsdeveloperblog.estore.ProductsService.query.rest.ProductRestModel;
 
 @Component
 public class ProductsQueryHandler {
@@ -11,6 +18,23 @@ public class ProductsQueryHandler {
 	
 	public ProductsQueryHandler(ProductsRepository productsRepository) {
 		this.productsRepository = productsRepository;
+	}
+	
+	@QueryHandler
+	public List<ProductRestModel> findProducts(FindProductsQuery query) {
+		
+		List<ProductRestModel> productsRest = new ArrayList<>();
+		
+		List<ProductEntity> storedProducts =  productsRepository.findAll();
+		
+		for(ProductEntity productEntity: storedProducts) {
+			ProductRestModel productRestModel = new ProductRestModel();
+			BeanUtils.copyProperties(productEntity, productRestModel);
+			productsRest.add(productRestModel);
+		}
+		
+		return productsRest;
+		
 	}
 
 }
