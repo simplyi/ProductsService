a0468d3520d6724dfdbd9c33c7322c42a6580f71
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-03-18 15:14:59 -0400

    Injected ProductsRepository interface into ProductsQueryHandler

--- src/main/java/com/appsdeveloperblog/estore/ProductsService/query/ProductsQueryHandler.java
+++ src/main/java/com/appsdeveloperblog/estore/ProductsService/query/ProductsQueryHandler.java
@@ -1,5 +1,16 @@
 package com.appsdeveloperblog.estore.ProductsService.query;
 
+import org.springframework.stereotype.Component;
+
+import com.appsdeveloperblog.estore.ProductsService.core.data.ProductsRepository;
+
+@Component
 public class ProductsQueryHandler {
+	
+	private final ProductsRepository productsRepository;
+	
+	public ProductsQueryHandler(ProductsRepository productsRepository) {
+		this.productsRepository = productsRepository;
+	}
 
 }
