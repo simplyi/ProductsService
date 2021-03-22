a6c639cc420e85b191dd41d85db53464c394bcf6
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-03-22 16:52:40 -0400

    Register the CreateProductCommandInterceptor on the CommandBus

--- src/main/java/com/appsdeveloperblog/estore/ProductsService/ProductsServiceApplication.java
+++ src/main/java/com/appsdeveloperblog/estore/ProductsService/ProductsServiceApplication.java
@@ -1,8 +1,13 @@
 package com.appsdeveloperblog.estore.ProductsService;
 
+import org.axonframework.commandhandling.CommandBus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
+
+import com.appsdeveloperblog.estore.ProductsService.command.interceptors.CreateProductCommandInterceptor;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,6 +15,13 @@ public class ProductsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductsServiceApplication.class, args);
+	}
+	
+	@Autowired
+	public void registerCreateProductCommandInterceptor(ApplicationContext context, 
+			CommandBus commandBus) {
+		commandBus.registerDispatchInterceptor(context.getBean(CreateProductCommandInterceptor.class));
+		
 	}
 
 }
