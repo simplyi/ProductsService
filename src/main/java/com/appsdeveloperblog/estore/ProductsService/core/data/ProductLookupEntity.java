9a52fcb7a5f7274cead3335f44ad56003097af7a
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-03-25 17:26:14 -0400

    Created ProductLookupEntity class

---
+++ src/main/java/com/appsdeveloperblog/estore/ProductsService/core/data/ProductLookupEntity.java
@@ -0,0 +1,28 @@
+package com.appsdeveloperblog.estore.ProductsService.core.data;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="productlookup")
+public class ProductLookupEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2788007460547645663L;
+	
+	@Id
+	private String productId;
+	
+	@Column(unique=true)
+	private String title;
+
+}
