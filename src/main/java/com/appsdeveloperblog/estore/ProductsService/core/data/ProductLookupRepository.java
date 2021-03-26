5086d2119332ccbbc758f126fbffe97915dad7bc
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-03-26 15:10:05 -0400

    Created ProductLookupRepository

---
+++ src/main/java/com/appsdeveloperblog/estore/ProductsService/core/data/ProductLookupRepository.java
@@ -0,0 +1,7 @@
+package com.appsdeveloperblog.estore.ProductsService.core.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductLookupRepository extends JpaRepository<ProductLookupEntity, String> {
+	ProductLookupEntity findByProductIdOrTitle(String productId, String title);
+}
