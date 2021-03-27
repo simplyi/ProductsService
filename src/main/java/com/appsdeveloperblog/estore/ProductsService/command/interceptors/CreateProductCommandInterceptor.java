36a7bd695ddc0b49608b0d090965b11b769ba4f0
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-03-27 09:36:03 -0400

    Updated CreateProductCommandInterceptor to check if the product exists

--- src/main/java/com/appsdeveloperblog/estore/ProductsService/command/interceptors/CreateProductCommandInterceptor.java
+++ src/main/java/com/appsdeveloperblog/estore/ProductsService/command/interceptors/CreateProductCommandInterceptor.java
@@ -11,12 +11,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.appsdeveloperblog.estore.ProductsService.command.CreateProductCommand;
+import com.appsdeveloperblog.estore.ProductsService.core.data.ProductLookupEntity;
+import com.appsdeveloperblog.estore.ProductsService.core.data.ProductLookupRepository;
 
 @Component
 public class CreateProductCommandInterceptor implements MessageDispatchInterceptor<CommandMessage<?>> {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(CreateProductCommandInterceptor.class);
-
+	private final ProductLookupRepository productLookupRepository;
+	
+	public CreateProductCommandInterceptor(ProductLookupRepository productLookupRepository) {
+		this.productLookupRepository = productLookupRepository;
+	}
+ 
 	@Override
 	public BiFunction<Integer, CommandMessage<?>, CommandMessage<?>> handle(
 			List<? extends CommandMessage<?>> messages) {
@@ -29,14 +36,16 @@ public class CreateProductCommandInterceptor implements MessageDispatchIntercept
 				
 				CreateProductCommand createProductCommand = (CreateProductCommand)command.getPayload();
 				
-				if(createProductCommand.getPrice().compareTo(BigDecimal.ZERO) <= 0) {
-					throw new IllegalArgumentException("Price cannot be less or equal than zero");
-				}
+				ProductLookupEntity productLookupEntity =  productLookupRepository.findByProductIdOrTitle(createProductCommand.getProductId(),
+						createProductCommand.getTitle());
 				
-				if(createProductCommand.getTitle() == null 
-						|| createProductCommand.getTitle().isBlank()) {
-					throw new IllegalArgumentException("Title cannot be empty");
+				if(productLookupEntity != null) {
+					throw new IllegalStateException(
+							String.format("Product with productId %s or title %s already exist", 
+									createProductCommand.getProductId(), createProductCommand.getTitle())
+							);
 				}
+
 			}
 			
 			return command;
