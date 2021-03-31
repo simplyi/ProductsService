f4bd3442f1ad79bc586abcc4dfdd2aa6156082b9
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-03-31 14:36:23 -0400

    Added CommandExecutionException to ProductsServiceErrorHandler

--- src/main/java/com/appsdeveloperblog/estore/ProductsService/core/errorhandling/ProductsServiceErrorHandler.java
+++ src/main/java/com/appsdeveloperblog/estore/ProductsService/core/errorhandling/ProductsServiceErrorHandler.java
@@ -2,6 +2,7 @@ package com.appsdeveloperblog.estore.ProductsService.core.errorhandling;
 
 import java.util.Date;
 
+import org.axonframework.commandhandling.CommandExecutionException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,13 @@ public class ProductsServiceErrorHandler {
 	
 	@ExceptionHandler(value= {Exception.class})
 	public ResponseEntity<Object> handleOtherExceptions(Exception ex, WebRequest request) {
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+		
+		return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(value= {CommandExecutionException.class})
+	public ResponseEntity<Object> handleCommandExecutionException(CommandExecutionException ex, WebRequest request) {
 		ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
 		
 		return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
