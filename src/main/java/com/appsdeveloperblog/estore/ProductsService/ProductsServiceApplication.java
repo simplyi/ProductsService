5d3a1b5daf8de8674c130112d99f70676f57e332
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-03-31 14:37:15 -0400

    Registered ProductsServiceEventsErrorHandler for the product-group processing group

--- src/main/java/com/appsdeveloperblog/estore/ProductsService/ProductsServiceApplication.java
+++ src/main/java/com/appsdeveloperblog/estore/ProductsService/ProductsServiceApplication.java
@@ -1,6 +1,8 @@
 package com.appsdeveloperblog.estore.ProductsService;
 
 import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventhandling.PropagatingErrorHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 
 import com.appsdeveloperblog.estore.ProductsService.command.interceptors.CreateProductCommandInterceptor;
+import com.appsdeveloperblog.estore.ProductsService.core.errorhandling.ProductsServiceEventsErrorHandler;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -22,6 +25,15 @@ public class ProductsServiceApplication {
 			CommandBus commandBus) {
 		commandBus.registerDispatchInterceptor(context.getBean(CreateProductCommandInterceptor.class));
 		
+	}
+	
+	@Autowired
+	public void configure(EventProcessingConfigurer config) {
+		config.registerListenerInvocationErrorHandler("product-group", 
+				conf -> new ProductsServiceEventsErrorHandler());
+		
+//		config.registerListenerInvocationErrorHandler("product-group", 
+//				conf -> PropagatingErrorHandler.instance());
 	}
 
 }
