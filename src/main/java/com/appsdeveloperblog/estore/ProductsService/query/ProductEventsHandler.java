2f102c65f6e092a3bb4661bc58a1252a5adbe443
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-03-31 14:34:25 -0400

    Added @ExceptionHandler methods to ProductEventsHandler class

--- src/main/java/com/appsdeveloperblog/estore/ProductsService/query/ProductEventsHandler.java
+++ src/main/java/com/appsdeveloperblog/estore/ProductsService/query/ProductEventsHandler.java
@@ -2,6 +2,7 @@ package com.appsdeveloperblog.estore.ProductsService.query;
 
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.messaging.interceptors.ExceptionHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
@@ -12,21 +13,36 @@ import com.appsdeveloperblog.estore.ProductsService.core.events.ProductCreatedEv
 @Component
 @ProcessingGroup("product-group")
 public class ProductEventsHandler {
-	
+
 	private final ProductsRepository productsRepository;
-	
+
 	public ProductEventsHandler(ProductsRepository productsRepository) {
 		this.productsRepository = productsRepository;
 	}
+	
+	@ExceptionHandler(resultType=Exception.class)
+	public void handle(Exception exception) throws Exception {
+		throw exception;
+	}
+	
+	@ExceptionHandler(resultType=IllegalArgumentException.class)
+	public void handle(IllegalArgumentException exception) {
+		// Log error message
+	}
+	
 
 	@EventHandler
 	public void on(ProductCreatedEvent event) {
-		
+
 		ProductEntity productEntity = new ProductEntity();
 		BeanUtils.copyProperties(event, productEntity);
-		
-		productsRepository.save(productEntity);
-		
+
+		try {
+			productsRepository.save(productEntity);
+		} catch (IllegalArgumentException ex) {
+			ex.printStackTrace();
+		}
+
 	}
-	
+
 }
