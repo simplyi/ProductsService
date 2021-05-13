d7cb4e29794563d893f2c799938affd082e9c654
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-05-13 16:16:23 -0400

    Triggering the Events Replay

--- src/main/java/com/appsdeveloperblog/estore/ProductsService/command/rest/EventsReplayController.java
+++ src/main/java/com/appsdeveloperblog/estore/ProductsService/command/rest/EventsReplayController.java
@@ -1,7 +1,11 @@
 package com.appsdeveloperblog.estore.ProductsService.command.rest;
 
+import java.util.Optional;
+
 import org.axonframework.config.EventProcessingConfiguration;
+import org.axonframework.eventhandling.TrackingEventProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +19,25 @@ public class EventsReplayController {
 	private EventProcessingConfiguration eventProcessingConfiguration;
 
 	@PostMapping("/eventProcessor/{processorName}/reset")
-	public void replayEvents(@PathVariable String processorName) {
+	public ResponseEntity<String> replayEvents(@PathVariable String processorName) {
 		
+		Optional<TrackingEventProcessor> trackingEventProcessor = 
+				eventProcessingConfiguration.eventProcessor(processorName, TrackingEventProcessor.class);
+		
+		if(trackingEventProcessor.isPresent()) {
+			TrackingEventProcessor eventProcessor = trackingEventProcessor.get();
+			eventProcessor.shutDown();
+			eventProcessor.resetTokens();
+			eventProcessor.start(); 
+			
+			return ResponseEntity.ok()
+					.body(String.format("The event processor with a name [%s] has been reset",
+					processorName));
+		} else {
+			return ResponseEntity.badRequest()
+					.body(String.format("The event processor with a name [%s] is not a tracking event processor."
+							+ " Only Tracking event processor is supported",processorName));
+		}
 	}
 	
 }
